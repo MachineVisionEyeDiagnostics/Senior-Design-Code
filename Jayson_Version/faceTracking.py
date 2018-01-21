f6c96b75c802ec56bbe70464e744fa341a477292
jbshiau235 <jskbmksh235@gmail.com>
2018-01-20 20:47:53 -0600

    Organized Code Versions
    - Each folder contains code specifically run on that user's computer
    - Makes it easier to update and test code on specific computers

---
+++ Jayson_Version/faceTracking.py
@@ -0,0 +1,43 @@
+import cv2
+
+FACE = 'C:\Users\Jayson\Documents\opencv3\opencv\sources\data\haarcascades\haarcascade_frontalface_default.xml'
+EYES = 'C:\Users\Jayson\Documents\opencv3\opencv\sources\data\haarcascades\haarcascade_eye.xml'
+
+webcam = cv2.VideoCapture(0)
+cv2.namedWindow('preview',cv2.WINDOW_NORMAL)
+cv2.resizeWindow('preview',500,500)
+face_cascade = cv2.CascadeClassifier(FACE)
+eye_cascade = cv2.CascadeClassifier(EYES)
+
+
+if webcam.isOpened(): # try to get the first frame
+    rval, frame = webcam.read()
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+else:
+    rval = False
+
+while rval:
+    faces = face_cascade.detectMultiScale(grey, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(grey,(x,y),(x+w,y+h),(0,0,0),2)
+        roi_grey = grey[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_grey)
+        for (ex,ey,ew,eh) in eyes[:2]:
+            cv2.rectangle(roi_grey,(ex,ey),(ex+ew,ey+eh),(0,0,0),2)
+
+    cv2.putText(grey, "Press ESC to close.", (5, 25),
+                cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,0,0))
+    cv2.imshow("preview", grey)
+
+    # get next frame
+    rval, frame = webcam.read()
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    key = cv2.waitKey(20)
+    if key in [27, ord('Q'), ord('q')]: # exit on ESC
+        break
+
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
