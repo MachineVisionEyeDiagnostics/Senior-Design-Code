c4de8fc92b7ac59c42684f740883ecaacb73bc3d
jbshiau235 <jskbmksh235@gmail.com>
2018-04-04 16:09:19 -0500

    Configured code to run on the Jetson TX2.

---
+++ c++_n/Code-for-Jetson/main.cpp
@@ -0,0 +1,376 @@
+#include "main_header.h"
+
+int main( int argc, const char** argv ) {
+    cv::Mat frame;
+    // Load the cascades
+    if(!face_cascade.load(face_cascade_name)){
+        printf("--(!)Error loading face cascade, please change face_cascade_name in source code.\n");
+        return -1;
+    }
+    //time1= std::chrono::steady_clock::now();
+    cv::VideoCapture capture(1);
+    if(capture.isOpened()){
+        while(1){
+            //int64 start = cv::getTickCount();
+            capture.read(frame);
+            // mirror the frame
+            //cv::flip(frame, frame, 1);
+            //frame.copyTo(debugImage);
+            // Apply the classifier to the frame
+            if(!frame.empty()){
+                detectAndDisplay( frame );
+            }
+            else{
+                printf(" --(!) No captured frame -- Break!");
+                break;
+            }
+            //double fps = cv::getTickFrequency()/(cv::getTickCount()-start);
+            //std::cout<<"FPS"<<fps<<std::endl;
+            int c = cv::waitKey(10);
+            if((char)c == ' '){
+                break;
+            }
+        }
+    }
+    
+    Mved.InitData(pointList, "/home/nvidia/Desktop/", "pupil_data.txt", ENABLE_DFT, "pupil_dft.txt", ENABLE_IMRECOGNIZER);
+
+    return 0;
+}
+
+
+void DecisionMaker::ImRecognizer()
+{
+    cv::Mat plot(cv::Size(250,250), CV_8U, 255);
+    for (unsigned int i = 0; i < pupil_data_xy_image.size(); i++)
+        plot.at<int>(pupil_data_xy_image[i]) = 0;
+    /*
+     * need to use erosion filter to enlarge the points and create a better image
+     */
+    cv::imshow("pupil data", plot);
+    cv::waitKey(10000);
+    cv::destroyAllWindows();
+}
+
+
+void DecisionMaker::WritePupilDft(std::string dft_file_name)
+{
+    // Need to log scale all data
+    Mved.file.open(paths+dft_file_name);
+    Mved.dtf_mat = cv::Mat((int)Mved.x_axis_pupil_points.size(),1,CV_64F,Mved.x_axis_pupil_points.data());
+    cv::dft(Mved.dtf_mat, Mved.complexI, cv::DFT_REAL_OUTPUT);
+    Mved.file<<Mved.complexI<<std::endl;
+    Mved.file.close();
+}
+ 
+void DecisionMaker::WritePupilData(std::string pupil_data_file_name)
+{
+    int n =  0;
+    Mved.file.open(paths+pupil_data_file_name);
+    for (auto i = pupil_points.begin(); i != pupil_points.end(); ++i)
+    {
+        double ptx = (((*i).x)-xEQ);
+        double pty = (((*i).y)-yEQ);
+        if(ENABLE_X_AXIS_PUPIL_POINTS)
+        {
+            x_axis_pupil_points.push_back(cv::Point((*i).x));
+        }
+        if(ENABLE_IMRECOGNIZER)
+        {
+            if( ((abs(ptx)-MIN_X_VAL) > 0) && ((abs(pty)-MIN_Y_VAL) > 0) )
+            {
+            pupil_data_xy_image.push_back(cv::Point(ptx+50,pty+50));
+            }
+        }
+        /*
+         * create some test case where it looks at the last value of ptx and pty with the current value.
+         * If ( ((ptx_previous - ptx_current)>SOME_VAL_X) && ((pty_previous - pty_current)>SOME_VAL_Y) )
+         * then we will consider this a value where the pupil location failed and do not want to consider it
+         * this will only be true for eye rolling where people typically obscure their eyes with their eyelid
+         */
+        Mved.file<<ptx<<'\t'<<n++<<'\t'<<pty<<'\t'<<(*i).z<<std::endl;
+    }
+    Mved.file.close();
+}
+
+void DecisionMaker::InitData(std::vector<cv::Point3d> pointList, std::string path,
+                             std::string pupil_data_file_name, bool dft,
+                             std::string dft_file_name,
+                             bool imrec)
+{
+    paths = path;
+    pupil_points = pointList;
+    ENABLE_X_AXIS_PUPIL_POINTS = ENABLE_DFT;
+    xEQ = (pupil_points.begin())->x;
+    yEQ = (pupil_points.begin())->y;
+    WritePupilData(pupil_data_file_name);
+    if(ENABLE_X_AXIS_PUPIL_POINTS)
+    {
+        WritePupilDft(dft_file_name);
+    }
+    if(ENABLE_IMRECOGNIZER)
+    {
+        ImRecognizer();
+    }
+    
+}
+
+
+
+void detectAndDisplay( cv::Mat frame ) {
+    std::vector<cv::Rect> faces;
+    std::vector<cv::Mat> rgbChannels(3);
+    cv::split(frame, rgbChannels);
+    cv::Mat frame_gray = rgbChannels[2];
+    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE|CV_HAAR_FIND_BIGGEST_OBJECT, cv::Size(150, 150) );
+    for( int i = 0; i < faces.size(); i++ )
+        rectangle(debugImage, faces[i], 1234);
+            //-- Show what you got
+    if (faces.size() > 0)
+        findEyes(frame_gray, faces[0]);
+}
+
+void findEyes(cv::Mat frame_gray, cv::Rect face) {
+    cv::Mat faceROI = frame_gray(face);
+    cv::Mat debugFace = faceROI;
+    //-- Find general eye region and draw cirlce over left pupil
+    int eye_region_width = face.width * (kEyePercentWidth/100.0);
+    int eye_region_height = face.width * (kEyePercentHeight/100.0);
+    int eye_region_top = face.height * (kEyePercentTop/100.0);
+    cv::Rect leftEyeRegion(face.width*(kEyePercentSide/100.0),eye_region_top,eye_region_width,eye_region_height);
+    cv::Point leftPupil = findEyeCenter(faceROI,leftEyeRegion);
+    leftPupil.x += leftEyeRegion.x;
+    leftPupil.y += leftEyeRegion.y;
+    #ifdef USE_RIGHT_EYE
+    std::cout<<"using right eye"<<std::endl;
+    cv::Rect rightEyeRegion(face.width - eye_region_width -face.width *(kEyePercentSide/100.0),
+                         eye_region_top,eye_region_width,eye_region_height);
+    cv::Point rightPupil = findEyeCenter(faceROI,rightEyeRegion);
+    rightPupil.x += rightEyeRegion.x;
+    rightPupil.y += rightEyeRegion.y;
+    cv::Point cyclops = {(rightPupil.x+leftPupil.x)/2,(rightPupil.y+leftPupil.y)/2};
+    auto time2= std::chrono::steady_clock::now();
+    std::chrono::duration<double, std::centi> dt = time2-time1;
+    pointList.push_back(cv::Point3d(cyclops.x,cyclops.y,dt.count())); //dt.count()
+    circle(debugFace, cyclops, 3, cvScalar(255));
+    #endif
+    pointList.push_back(cv::Point3d(leftPupil.x,leftPupil.y,1));
+    circle(debugFace, leftPupil, 3, cvScalar(255));
+    //std::cout<<"movement of eye: "<<   <<std::endl;
+    imshow(face_window_name, debugFace);
+    //time1 = std::chrono::steady_clock::now();
+}
+
+
+cv::Mat floodKillEdges(cv::Mat &mat);
+
+cv::Point unscalePoint(cv::Point p, cv::Rect origSize) {
+    float ratio = (((float)kFastEyeWidth)/origSize.width);
+    int x = round(p.x / ratio);
+    int y = round(p.y / ratio);
+    return cv::Point(x,y);
+}
+
+void scaleToFastSize(const cv::Mat &src,cv::Mat &dst) {
+    cv::resize(src, dst, cv::Size(kFastEyeWidth,(((float)kFastEyeWidth)/src.cols) * src.rows),cv::INTER_LINEAR );
+}
+
+cv::Mat computeMatXGradient(const cv::Mat &mat) {
+    cv::Mat out(mat.rows,mat.cols,CV_64F);
+    
+    for (int y = 0; y < mat.rows; ++y) {
+        const uchar *Mr = mat.ptr<uchar>(y);
+        double *Or = out.ptr<double>(y);
+        
+        Or[0] = Mr[1] - Mr[0];
+        for (int x = 1; x < mat.cols - 1; ++x) {
+            Or[x] = (Mr[x+1] - Mr[x-1])/2.0;
+        }
+        Or[mat.cols-1] = Mr[mat.cols-1] - Mr[mat.cols-2];
+    }
+    
+    return out;
+}
+
+
+void testPossibleCentersFormula(int x, int y, const cv::Mat &weight,double gx, double gy, cv::Mat &out) {
+    // for all possible centers
+    for (int cy = 0; cy < out.rows; ++cy) {
+        double *Or = out.ptr<double>(cy);
+        const unsigned char *Wr = weight.ptr<unsigned char>(cy);
+        for (int cx = 0; cx < out.cols; ++cx) {
+            if (x == cx && y == cy) {
+                continue;
+            }
+            // create a vector from the possible center to the gradient origin
+            double dx = x - cx;
+            double dy = y - cy;
+            // normalize d
+            double magnitude = (sqrt((dx * dx) + (dy * dy)));
+            dx = dx/magnitude;
+            dy = dy/magnitude;
+            double dotProduct = abs(dx*gx + dy*gy);
+            dotProduct = std::max(0.0,dotProduct);
+            // square and multiply by the weight
+            Or[cx] += 2*dotProduct * (Wr[cx]/kWeightDivisor);
+        }
+    }
+}
+
+cv::Point findEyeCenter(cv::Mat face, cv::Rect eye) {
+    cv::Mat eyeROIUnscaled = face(eye);
+    cv::Mat eyeROI;
+    
+     scaleToFastSize(eyeROIUnscaled, eyeROI);
+    // draw eye region
+    //rectangle(face,eye,1234);
+    //-- Find the gradient
+    cv::Mat gradientX = computeMatXGradient(eyeROI);
+    cv::Mat gradientY = computeMatXGradient(eyeROI.t()).t();
+    //-- Normalize and threshold the gradient
+    // compute all the magnitudes
+    
+    cv::Mat mags = matrixMagnitude(gradientX, gradientY);
+    //compute the threshold
+    
+    double gradientThresh = computeDynamicThreshold(mags, kGradientThreshold);
+    //double gradientThresh = kGradientThreshold;
+    //double gradientThresh = 0;
+    //normalize
+    for (int y = 0; y < eyeROI.rows; ++y) {
+        double *Xr = gradientX.ptr<double>(y), *Yr = gradientY.ptr<double>(y);
+        const double *Mr = mags.ptr<double>(y);
+        for (int x = 0; x < eyeROI.cols; ++x) {
+            double gX = Xr[x], gY = Yr[x];
+            double magnitude = Mr[x];
+            if (magnitude > gradientThresh) {
+                Xr[x] = gX/magnitude;
+                Yr[x] = gY/magnitude;
+            } else {
+                Xr[x] = 0.0;
+                Yr[x] = 0.0;
+            }
+        }
+    }
+    //cv::String debugWindow = "debugwindow";
+    //imshow(debugWindow,gradientX);
+    //-- Create a blurred and inverted image for weighting
+    cv::Mat weight;
+    
+    GaussianBlur( eyeROI, weight, cv::Size( kWeightBlurSize, kWeightBlurSize ), 0, 0 );
+    for (int y = 0; y < weight.rows; ++y) {
+        unsigned char *row = weight.ptr<unsigned char>(y);
+        for (int x = 0; x < weight.cols; ++x) {
+            row[x] = (255 - row[x]);
+        }
+    }
+    //imshow( "Display window", weight);
+    //-- Run the algorithm!
+    cv::Mat outSum = cv::Mat::zeros(eyeROI.rows,eyeROI.cols,CV_64F);
+    // for each possible gradient location
+    // Note: these loops are reversed from the way the paper does them
+    // it evaluates every possible center for each gradient location instead of
+    // every possible gradient location for every center.
+    //printf("Eye Size: %ix%i\n",outSum.cols,outSum.rows);
+    for (int y = 0; y < weight.rows; ++y) {
+        const double *Xr = gradientX.ptr<double>(y), *Yr = gradientY.ptr<double>(y);
+       
+        for (int x = 0; x < weight.cols; ++x) {
+            double gX = Xr[x], gY = Yr[x];
+            if (gX == 0.0 && gY == 0.0) {
+                continue;
+            }
+            
+            testPossibleCentersFormula(x, y, weight, gX, gY, outSum);
+        }
+    }
+    // scale all the values down, basically averaging them
+    double numGradients = (weight.rows*weight.cols);
+    cv::Mat out;
+    outSum.convertTo(out, CV_32F,1.0/numGradients);
+    //imshow(debugWindow,out);
+    //-- Find the maximum point
+    cv::Point maxP;
+    double maxVal;
+    
+    cv::minMaxLoc(out, NULL,&maxVal,NULL,&maxP);
+    //-- Flood fill the edges
+    if(kEnablePostProcess) {
+        cv::Mat floodClone;
+        //double floodThresh = computeDynamicThreshold(out, 1.5);
+        double floodThresh = maxVal * kPostProcessThreshold;
+        cv::threshold(out, floodClone, floodThresh, 0.0f, cv::THRESH_TOZERO);
+        cv::Mat mask = floodKillEdges(floodClone);
+        // redo max
+        cv::minMaxLoc(out, NULL,&maxVal,NULL,&maxP,mask);
+    }
+    return unscalePoint(maxP,eye);
+}
+
+//#pragma mark Postprocessing
+
+bool floodShouldPushPoint(const cv::Point &np, const cv::Mat &mat) {
+    return inMat(np, mat.rows, mat.cols);
+}
+
+// returns a mask
+cv::Mat floodKillEdges(cv::Mat &mat) {
+    rectangle(mat,cv::Rect(0,0,mat.cols,mat.rows),255);
+    
+    cv::Mat mask(mat.rows, mat.cols, CV_8U, 255);
+    std::queue<cv::Point> toDo;
+    toDo.push(cv::Point(0,0));
+    while (!toDo.empty()) {
+        cv::Point p = toDo.front();
+        toDo.pop();
+        if (mat.at<float>(p) == 0.0f) {
+            continue;
+        }
+        // add in every direction
+        cv::Point np(p.x + 1, p.y); // right
+        if (floodShouldPushPoint(np, mat)) toDo.push(np);
+        np.x = p.x - 1; np.y = p.y; // left
+        if (floodShouldPushPoint(np, mat)) toDo.push(np);
+        np.x = p.x; np.y = p.y + 1; // down
+        if (floodShouldPushPoint(np, mat)) toDo.push(np);
+        np.x = p.x; np.y = p.y - 1; // up
+        if (floodShouldPushPoint(np, mat)) toDo.push(np);
+        // kill it
+        mat.at<float>(p) = 0.0f;
+        mask.at<uchar>(p) = 0;
+    }
+    return mask;
+}
+
+
+bool rectInImage(cv::Rect rect, cv::Mat image) {
+    return rect.x > 0 && rect.y > 0 && rect.x+rect.width < image.cols &&
+    rect.y+rect.height < image.rows;
+}
+
+bool inMat(cv::Point p,int rows,int cols) {
+    return p.x >= 0 && p.x < cols && p.y >= 0 && p.y < rows;
+}
+
+cv::Mat matrixMagnitude(const cv::Mat &matX, const cv::Mat &matY) {
+    cv::Mat mags(matX.rows,matX.cols,CV_64F);
+    for (int y = 0; y < matX.rows; ++y) {
+        const double *Xr = matX.ptr<double>(y), *Yr = matY.ptr<double>(y);
+        double *Mr = mags.ptr<double>(y);
+        for (int x = 0; x < matX.cols; ++x) {
+            double gX = Xr[x], gY = Yr[x];
+            double magnitude = sqrt((gX * gX) + (gY * gY));
+            Mr[x] = magnitude;
+        }
+    }
+    return mags;
+}
+
+double computeDynamicThreshold(const cv::Mat &mat, double stdDevFactor) {
+    cv::Scalar stdMagnGrad, meanMagnGrad;
+    cv::meanStdDev(mat, meanMagnGrad, stdMagnGrad);
+    double stdDev = stdMagnGrad[0] / sqrt(mat.rows*mat.cols);
+    return stdDevFactor * stdDev + meanMagnGrad[0];
+}
+
+
---
+++ c++_n/Code-for-Jetson/main_header.h
@@ -0,0 +1,115 @@
+//
+//  main_header.h
+//  c++_practice
+//
+//  Created by NICK NORDEN on 2/28/18.
+//  Copyright © 2018 NICK NORDEN. All rights reserved.
+//
+
+#ifndef main_header_h
+#define main_header_h
+
+
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/plot.hpp>
+
+
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <chrono>
+
+#define ENABLE_DFT true
+#define DISABLE_DFT false
+#define ENABLE_IMRECOGNIZER true
+#define DISABLE_IMRECOGNIZER false
+#define MIN_X_VAL 5
+#define MIN_Y_VAL 5
+
+/** Function Headers */
+void detectAndDisplay( cv::Mat frame );
+void findEyes(cv::Mat, cv::Rect);
+bool rectInImage(cv::Rect rect, cv::Mat image);
+bool inMat(cv::Point p,int rows,int cols);
+cv::Mat matrixMagnitude(const cv::Mat &matX, const cv::Mat &matY);
+double computeDynamicThreshold(const cv::Mat &mat, double stdDevFactor);
+cv::Point findEyeCenter(cv::Mat face, cv::Rect eye);
+void pupilPrint(std::vector<cv::Point3d> pp);
+// Algorithm Parameters
+const int kFastEyeWidth = 50;
+const int kWeightBlurSize = 5;
+const float kWeightDivisor = 1.0;
+const double kGradientThreshold = 50.0;
+const int kEyePercentTop = 25;
+const int kEyePercentSide = 13;
+const int kEyePercentHeight = 30;
+const int kEyePercentWidth = 35;
+
+// Postprocessing
+const bool kEnablePostProcess = true;
+const float kPostProcessThreshold = 0.90;
+
+/** Global variables */
+//-- Note, either copy these two files from opencv/data/haarscascades to your current folder, or change these locations
+const cv::String face_cascade_name = "haarcascade_frontalface_alt.xml";
+cv::CascadeClassifier face_cascade;
+const std::string face_window_name = "leftPupil";
+const cv::RNG rng(12345);
+cv::Mat debugImage;
+std::vector<cv::Point3d> pointList;
+
+class DecisionMaker {
+public:
+    std::string paths;
+    std::ofstream file;
+    void ImRecognizer(void);
+    void WritePupilDft(std::string dft_file_name);
+    void WritePupilData(std::string pupil_data_file_name);
+    void InitData(std::vector<cv::Point3d> pointList, std::string path, std::string pupil_data_file_name, bool enable_dft, std::string dft_file_name, bool enable_imrec);
+private:
+    cv::Mat dtf_mat;
+    cv::Mat complexI;
+    std::vector<cv::Point3d> pupil_points;
+    std::vector<cv::Point> x_axis_pupil_points;
+    std::vector<cv::Point> pupil_data_xy_image;
+    bool ENABLE_X_AXIS_PUPIL_POINTS;
+    double xEQ;
+    double yEQ;
+};
+
+DecisionMaker Mved;
+//std::chrono::steady_clock::time_point time1;
+
+#endif /* main_header_h */
+
+
+/*
+ 
+ void pupilPrint(std::vector<cv::Point3d> pp){
+ std::ofstream myFile;
+ myFile.open("/Users/nicknorden/Desktop/pupil_data.txt");
+ int n = 0;
+ auto xEQ = (pp.begin())->x;
+ auto yEQ = (pp.begin())->y;
+ for (auto i = pp.begin(); i != pp.end(); ++i){
+ split_x.push_back(cv::Point((*i).x));
+ double ptx = (((*i).x)-xEQ);
+ double pty= (((*i).y)-yEQ);
+ myFile<<ptx<<'\t'<<n++<<'\t'<<pty<<std::endl;
+ }
+ cv::Mat dtf_mat = cv::Mat((int)split_x.size(),1,CV_64F,split_x.data());
+ 
+ //cv::Mat planes[] = {cv::Mat_<float>(dtf_mat), cv::Mat::zeros(dtf_mat.size(), CV_32F)};
+ cv::Mat complexI;    //Complex plane to contain the DFT coefficients {[0]-Real,[1]-Img}
+ //cv::merge(planes, 2, complexI);
+ cv::dft(dtf_mat, complexI, cv::DFT_REAL_OUTPUT);
+ std::cout<<complexI<<std::endl;
+ myFile.close();
+ 
+ }
+*/
