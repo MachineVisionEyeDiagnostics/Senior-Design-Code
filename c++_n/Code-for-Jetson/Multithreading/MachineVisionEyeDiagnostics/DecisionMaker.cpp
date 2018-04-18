67f4b97b0cda6b94b9ffaf4e32ef47f47b5efc4d
NICK NORDEN <nicknorden@nicks-air-2>
2018-04-18 00:11:18 -0500

    removed extra code

--- c++_n/Code-for-Jetson/Multithreading/MachineVisionEyeDiagnostics/DecisionMaker.cpp
+++ c++_n/Code-for-Jetson/Multithreading/MachineVisionEyeDiagnostics/DecisionMaker.cpp
@@ -1,5 +1,6 @@
 #include "DecisionMaker.h"
-#include "circleFit.h"
+#define PI 3.14159265
+
 
 /*
 struct {
@@ -80,10 +81,9 @@ void::DecisionMaker::ImRecognizer()
                (TopMidPoint.y-square.center.y)*(TopMidPoint.y-square.center.y)));
     float b = sqrt(((RightMidPoint.x-square.center.x)*(RightMidPoint.x-square.center.x) +
                (RightMidPoint.y-square.center.y)*(RightMidPoint.y-square.center.y)));
-    cout<<a<<"::"<<b<<"";
     float A,B,C,D,E,F;
-    float sin_theta = sin(square.angle*Pi/180);
-    float cos_theta = cos(square.angle*Pi/180);
+    float sin_theta = sin(square.angle*PI/180);
+    float cos_theta = cos(square.angle*PI/180);
     
     if(a>b){
         A = a*a*sin_theta*sin_theta+b*b*cos_theta*cos_theta;
@@ -94,7 +94,7 @@ void::DecisionMaker::ImRecognizer()
         F =A*square.center.x*square.center.x+B*square.center.x*square.center.y+C*square.center.y*square.center.y-a*a*b*b;
     }
     else{
-        swap(a,b);
+        cv::swap(a,b);
         A = a*a*sin_theta*sin_theta+b*b*cos_theta*cos_theta;
         B = 2*(b*b-a*a)*sin_theta*cos_theta;
         C = a*a*cos_theta*cos_theta+b*b*sin_theta*sin_theta;
@@ -108,18 +108,18 @@ void::DecisionMaker::ImRecognizer()
 
     for(auto i:pupil_data_xy_image){
         float point = A*i.x*i.x+B*i.x*i.y+C*i.y*i.y+D*i.x+E*i.y+F;
-        cout<<point<<endl;
+        std::cout<<point<<std::endl;
         if(point<=0)
             inside++;
         else
             outside++;
     }
     float result = inside/pupil_data_xy_image.size();
-    cout<<result;
+    std::cout<<result;
     if(result > 0.5)
-        cout<<"eye roll passed";
+        std::cout<<"eye roll passed";
     else
-        cout<<"eye roll failed";
+        std::cout<<"eye roll failed";
 
 
     
@@ -296,146 +296,5 @@ void DecisionMaker::InitData(std::vector<cv::Point3d> pointList, std::string pat
         ImRecognizer();
     }
     
-}
-
-Circle CircleFitByTaubin (Data& data)
-/*
- Circle fit to a given set of data points (in 2D)
- 
- This is an algebraic fit, due to Taubin, based on the journal article
- 
- G. Taubin, "Estimation Of Planar Curves, Surfaces And Nonplanar
- Space Curves Defined By Implicit Equations, With
- Applications To Edge And Range Image Segmentation",
- IEEE Trans. PAMI, Vol. 13, pages 1115-1138, (1991)
- 
- Input:  data     - the class of data (contains the given points):
- 
- data.n   - the number of data points
- data.X[] - the array of X-coordinates
- data.Y[] - the array of Y-coordinates
- 
- Output:
- circle - parameters of the fitting circle:
- 
- circle.a - the X-coordinate of the center of the fitting circle
- circle.b - the Y-coordinate of the center of the fitting circle
- circle.r - the radius of the fitting circle
- circle.s - the root mean square error (the estimate of sigma)
- circle.j - the total number of iterations
- 
- The method is based on the minimization of the function
- 
- sum [(x-a)^2 + (y-b)^2 - R^2]^2
- F = -------------------------------
- sum [(x-a)^2 + (y-b)^2]
- 
- This method is more balanced than the simple Kasa fit.
- 
- It works well whether data points are sampled along an entire circle or
- along a small arc.
- 
- It still has a small bias and its statistical accuracy is slightly
- lower than that of the geometric fit (minimizing geometric distances),
- but slightly higher than that of the very similar Pratt fit.
- Besides, the Taubin fit is slightly simpler than the Pratt fit
- 
- It provides a very good initial guess for a subsequent geometric fit.
- 
- Nikolai Chernov  (September 2012)
- 
- */
-{
-    int i,iter,IterMAX=99;
-    
-    reals Xi,Yi,Zi;
-    reals Mz,Mxy,Mxx,Myy,Mxz,Myz,Mzz,Cov_xy,Var_z;
-    reals A0,A1,A2,A22,A3,A33;
-    reals Dy,xnew,x,ynew,y;
-    reals DET,Xcenter,Ycenter;
-    
-    Circle circle;
-    
-    data.means();   // Compute x- and y- sample means (via a function in the class "data")
-    
-    //     computing moments
-    
-    Mxx=Myy=Mxy=Mxz=Myz=Mzz=0.;
-    
-    for (i=0; i<data.n; i++)
-    {
-        Xi = data.X[i] - data.meanX;   //  centered x-coordinates
-        Yi = data.Y[i] - data.meanY;   //  centered y-coordinates
-        Zi = Xi*Xi + Yi*Yi;
-        
-        Mxy += Xi*Yi;
-        Mxx += Xi*Xi;
-        Myy += Yi*Yi;
-        Mxz += Xi*Zi;
-        Myz += Yi*Zi;
-        Mzz += Zi*Zi;
-    }
-    Mxx /= data.n;
-    Myy /= data.n;
-    Mxy /= data.n;
-    Mxz /= data.n;
-    Myz /= data.n;
-    Mzz /= data.n;
-    
-    //      computing coefficients of the characteristic polynomial
-    
-    Mz = Mxx + Myy;
-    Cov_xy = Mxx*Myy - Mxy*Mxy;
-    Var_z = Mzz - Mz*Mz;
-    A3 = Four*Mz;
-    A2 = -Three*Mz*Mz - Mzz;
-    A1 = Var_z*Mz + Four*Cov_xy*Mz - Mxz*Mxz - Myz*Myz;
-    A0 = Mxz*(Mxz*Myy - Myz*Mxy) + Myz*(Myz*Mxx - Mxz*Mxy) - Var_z*Cov_xy;
-    A22 = A2 + A2;
-    A33 = A3 + A3 + A3;
-    
-    //    finding the root of the characteristic polynomial
-    //    using Newton's method starting at x=0
-    //     (it is guaranteed to converge to the right root)
-    
-    for (x=0.,y=A0,iter=0; iter<IterMAX; iter++)  // usually, 4-6 iterations are enough
-    {
-        Dy = A1 + x*(A22 + A33*x);
-        xnew = x - y/Dy;
-        if ((xnew == x)||(!isfinite(xnew))) break;
-        ynew = A0 + xnew*(A1 + xnew*(A2 + xnew*A3));
-        if (abs(ynew)>=abs(y))  break;
-        x = xnew;  y = ynew;
-    }
-    
-    //       computing paramters of the fitting circle
-    
-    DET = x*x - x*Mz + Cov_xy;
-    Xcenter = (Mxz*(Myy - x) - Myz*Mxy)/DET/Two;
-    Ycenter = (Myz*(Mxx - x) - Mxz*Mxy)/DET/Two;
-    
-    //       assembling the output
-    
-    circle.a = Xcenter + data.meanX;
-    circle.b = Ycenter + data.meanY;
-    circle.r = sqrt(Xcenter*Xcenter + Ycenter*Ycenter + Mz);
-    circle.s = Sigma(data,circle);
-    circle.i = 0;
-    circle.j = iter;  //  return the number of iterations, too
-    
-    return circle;
-}
-
-reals Sigma (Data& data, Circle& circle)
-{
-    reals sum=0.,dx,dy;
-    
-    for (int i=0; i<data.n; i++)
-    {
-        dx = data.X[i] - circle.a;
-        dy = data.Y[i] - circle.b;
-        sum += SQR(sqrt(dx*dx+dy*dy) - circle.r);
-    }
-    return sqrt(sum/data.n);
 }
 
